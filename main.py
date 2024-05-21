2317a08b68da3e2cf1e4338c00a5845a6da79bfa
Moahmmed Alagha <modyagha7@gmail.com>
2024-05-21 11:32:32 +0300

    init

---
+++ main.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langserve import add_routes
+
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+model = ChatOpenAI(
+    model_name="gpt-3.5-turbo",
+    api_key= "OPENAI_API_KEY",
+)
+
+parser = StrOutputParser()
+
+chain = prompt_template | model | parser
+
+
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
